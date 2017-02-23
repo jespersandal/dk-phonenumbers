c109d7f2211abbc1346cc7e0756b3c531f1233ee
Jesper S. Sandal <jespersandal@outlook.com>
2017-02-23 11:50:29 +0100

    initial commit

---
+++ facephone.py
@@ -0,0 +1,10 @@
+import sys
+
+print('Argumenter:', str(sys.argv))
+
+def makelist(start, end, prefix, suffix)
+    start = int(start)
+    end = int(end)
+    prefix = int(prefix)
+    suffix = int(suffix)
+    
